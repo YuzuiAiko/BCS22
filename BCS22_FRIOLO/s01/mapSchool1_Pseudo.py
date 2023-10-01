aabef36cad1b732ff67de9ae7ca9bd40221124f5
YuzuiAiko <142853089+YuzuiAiko@users.noreply.github.com>
2023-10-01 12:42:59 +0800

    Update mapSchool1_Pseudo.py

--- BCS22_FRIOLO/s01/mapSchool1_Pseudo.py
+++ BCS22_FRIOLO/s01/mapSchool1_Pseudo.py
@@ -22,7 +22,7 @@
 #                                                    |                        |
 #                		                                          Update previous_loc[next_loc].
 #                                                    |
-#    				                    While end is not equal to start:(Inside Below)
+#    				          While end is not equal to start:(Inside Below)
 #                                                    |                               |
 #        			                                              Append end to the shortpath_graph.
 #                                                    |                               |
@@ -30,8 +30,8 @@
 #                                                    |                               |
 #        		                                                 If start is equal to end, append start to the shortpath_graph.
 #                                                    |
-#    					                  Reverse the shortpath_graph.
+#    					        Reverse the shortpath_graph.
 #                                                                     |
 #    		                Print "The shortest path from home to school (L to R):", followed by the contents of shortpath_graph.
 #                                                                     |
-#  						                                               End.
+#  						                                    End.
