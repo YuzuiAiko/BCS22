cb5d77827f2110321753d1956e0e50130316a7df
YuzuiAiko <142853089+YuzuiAiko@users.noreply.github.com>
2023-12-15 10:31:57 +0800

    Update Final_LabExam.py

--- Final_LabExam.py
+++ Final_LabExam.py
@@ -11,13 +11,10 @@ def vertical_order_traversal(root):
     if not root:
         return result
 
-
     vertical_dict = defaultdict(list)
-
 
     queue = deque()
     distance_queue = deque()
-
 
     queue.append(root)
     distance_queue.append(0)
@@ -26,25 +23,20 @@ def vertical_order_traversal(root):
         current = queue.popleft()
         distance = distance_queue.popleft()
 
-
         vertical_dict[distance].append(current.val)
-
 
         if current.left:
             queue.append(current.left)
             distance_queue.append(distance - 1)
 
-
         if current.right:
             queue.append(current.right)
             distance_queue.append(distance + 1)
 
-
-    for values in sorted(vertical_dict.items(), reverse=True):
+    for values in sorted(vertical_dict.items()):
         result.extend(values[1])
 
-    return result
-
+    return result[::-1]
 
 root = TreeNode(1)
 root.left = TreeNode(2)
@@ -56,8 +48,5 @@ root.right.right = TreeNode(7)
 root.right.left.right = TreeNode(8)
 root.right.right.right = TreeNode(9)
 
-
 result = vertical_order_traversal(root)
-
-
 print(result)
